1e69049d030ef12abb9a382ed3d2e2f8d2aa1a74
Gilbert Torchon <gilbert@noukod.com>
2019-01-22 16:43:58 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class TitleValidator < ActiveModel::EachValidator
     # if !rightValues.include?(value)
     #   return false
     # end
-    unless value.nil? || value.scan(/Won't Believe|Secret|To[number]|Guess/)!=[]
+    unless !value.nil? && value.scan(/Won't Believe|Secret|To[number]|Guess/)!=[]
       record.errors[attribute] << (options[:message] || "is not a valid one")
     end
   end
@@ -15,7 +15,7 @@ end
 
 
 class Post < ActiveRecord::Base
-  validates :title,title:true,presence:true
+  validates :title,title:true
   validates :content,length:{ minimum: 10}
   validates :summary,length:{ maximum: 25}
   validates :category,inclusion:{ in:%w(Fiction Non-Fiction)}
