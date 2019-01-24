bb5bb75bc9f7f1c8b9d66d7fb2b470bd32459251
Keren-Happuc Fleurimond <keren@noukod.com>
2019-01-24 18:05:32 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,19 @@
+class PostTitleValidator < ActiveModel::Validator
+  def validate(record)
+    clickbatArr = ["Won't Believe", "Secret", "Top", "Guess"]
+    if record.title.present?
+      unless clickbatArr.any? { |w| record.title =~ /#{w}/ }
+        record.errors[:title] << "Invalid title"
+      end
+    end
+  end
+end
+
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+  validates_with PostTitleValidator
 end
