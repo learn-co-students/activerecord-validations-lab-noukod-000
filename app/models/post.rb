790277c741d10079d9226ae073c146b063baccaa
Merline Belizaire <merline@noukod.com>
2019-01-29 20:31:05 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ActiveRecord::Base
+  validates :title,title:true
+  validates :content,length:{ minimum: 10}
+  validates :summary,length:{ maximum: 25}
+  validates :category,inclusion:{ in:%w(Fiction Non-Fiction)}
+end
+
+class TitleValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless !value.nil? && value.scan(/Won't Believe|Secret|To[number]|Guess/)!=[]
+      record.errors[attribute] << (options[:message] || "is not a valid one")
+    end
+  end
 end
