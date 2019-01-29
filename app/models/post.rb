43572dfdd7b0e41db8dd905d198abbf4d4ffb6f8
Merline Belizaire <merline@noukod.com>
2019-01-29 20:32:47 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,21 @@
-class Post < ActiveRecord::Base
-  validates :title,title:true
-  validates :content,length:{ minimum: 10}
-  validates :summary,length:{ maximum: 25}
-  validates :category,inclusion:{ in:%w(Fiction Non-Fiction)}
-end
 
-class TitleValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless !value.nil? && value.scan(/Won't Believe|Secret|To[number]|Guess/)!=[]
-      record.errors[attribute] << (options[:message] || "is not a valid one")
+class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validate :is_clickbait?
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i
+  ]
+
+  def is_clickbait?
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+      errors.add(:title, "must be clickbait")
     end
   end
 end
