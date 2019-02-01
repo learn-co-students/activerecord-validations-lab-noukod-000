c5f783b5621de0fa6300a47956bb50875942bfad
Saint-Jean Germila <germila@noukod.com>
2019-02-01 21:21:24 +0000

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,5 @@
 class Author < ActiveRecord::Base
-  # validates :name, phone_number: true
+  validates :name, phone_number: true
 end
-a = Author.new
+Author.create(name: "John Doe").valid? # => true
+Author.create(name: nil).valid?
